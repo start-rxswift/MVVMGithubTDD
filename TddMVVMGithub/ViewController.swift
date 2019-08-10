db9e53427a3ec2fc57cf48eeab62c5690aa71a6e
kimtaesu <kimtaesoo188@gmail.com>
2019-08-10 09:09:03 +0900

    First init

---
+++ TddMVVMGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TddMVVMGithub
+//
+//  Created by tskim on 10/08/2019.
+//  Copyright © 2019 hucet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
